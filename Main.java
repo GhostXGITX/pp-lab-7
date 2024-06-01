48792af950c2021099e9f8603273383409b6839f
Mikolaj <mikolaj.m.zulewski@gmail.com>
2024-06-01 12:35:42 +0200

    zad3

--- Main.java
+++ Main.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class Main extends Application {
 
@@ -34,9 +37,11 @@ public class Main extends Application {
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
+        vBox.setStyle("-fx-padding: 10; -fx-alignment: center;");
 
         Scene scene = new Scene(vBox, 600, 200);
         primaryStage.setScene(scene);
@@ -49,6 +54,37 @@ public class Main extends Application {
         File selectedDirectory = directoryChooser.showDialog(null);
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        if(directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory =new File(directoryPath);
+        if(!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFilesInDirectory(directory, results);
+                } else {
+                    results.append(file.getAbsolutePath()).append("\n");
+                }
+            }
         }
     }
 
